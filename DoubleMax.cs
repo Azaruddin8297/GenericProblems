6e47b23cf334208fac3e0c0c7e4cba50c7ec48ab
Azaruddin8297 <125579015+Azaruddin8297@users.noreply.github.com>
2023-03-24 17:15:45 +0530

    DoubleMax

---
+++ DoubleMax.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericProb
+{
+    public class DoubleMax
+    {
+        public static double MaxNumber(double FirstNumber, double SecondNumber, double ThirdNumber)
+        {
+            {
+                if (FirstNumber.CompareTo(SecondNumber) > 0 && FirstNumber.CompareTo(ThirdNumber) > 0 ||
+                    FirstNumber.CompareTo(SecondNumber) >= 0 && FirstNumber.CompareTo(ThirdNumber) > 0 ||
+                    FirstNumber.CompareTo(SecondNumber) > 0 && FirstNumber.CompareTo(ThirdNumber) >= 0)
+                {
+                    return FirstNumber;
+                }
+                if (SecondNumber.CompareTo(FirstNumber) > 0 && SecondNumber.CompareTo(ThirdNumber) > 0 ||
+                    SecondNumber.CompareTo(FirstNumber) >= 0 && SecondNumber.CompareTo(ThirdNumber) > 0 ||
+                    SecondNumber.CompareTo(FirstNumber) > 0 && SecondNumber.CompareTo(ThirdNumber) >= 0)
+                {
+                    return SecondNumber;
+                }
+                if (ThirdNumber.CompareTo(SecondNumber) > 0 && ThirdNumber.CompareTo(FirstNumber) > 0 ||
+                    ThirdNumber.CompareTo(SecondNumber) >= 0 && ThirdNumber.CompareTo(FirstNumber) > 0 ||
+                    ThirdNumber.CompareTo(SecondNumber) > 0 && ThirdNumber.CompareTo(FirstNumber) >= 0)
+                {
+                    return ThirdNumber;
+                }
+                return FirstNumber;
+            }
+        }
+    }
+}
